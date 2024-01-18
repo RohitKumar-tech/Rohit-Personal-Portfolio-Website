7020bdc59a893d6cfee608c04a5b25568994d687
Rohit kumar <73387772+RohitKumar-tech@users.noreply.github.com>
2024-01-18 23:17:24 +0530

    updated the code of the website

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// toggle icon navbar
+
+// scroll selections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
