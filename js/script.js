82e37560724c170c3a70ba9aa2566341678970cc
Rohit kumar <73387772+RohitKumar-tech@users.noreply.github.com>
2024-01-19 20:06:47 +0530

    Todays-Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll selections
 let sections = document.querySelectorAll('section');
@@ -17,10 +24,20 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate');
         }
     });
     // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
